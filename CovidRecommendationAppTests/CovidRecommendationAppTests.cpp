436a8581a5ff389051b220e340833a67600ab28f
Sam Lewis <sam.lewis@enett.com>
2021-10-10 20:39:13 +1100

    Adding initial solution with the main project and unit test project

---
+++ CovidRecommendationAppTests/CovidRecommendationAppTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CovidRecommendationAppTests
+{
+	TEST_CLASS(CovidRecommendationAppTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
