1079d24d9a9d2aa296f267bf77c3d5bb0edaafc5
Sam Lewis <sam.lewis@enett.com>
2021-10-17 14:06:33 +1100

    Added database insert functionality

--- CovidRecommendationApp/Database.cpp
+++ CovidRecommendationApp/Database.cpp
@@ -21,10 +21,47 @@ void Database::createTable(string file, vector<string> headers)
 	{
 		for (int i = 0; i < headers.size(); i++)
 		{
-			datafile << headers[i] << '\t';
+			datafile << headers[i] << ',';
 		}
 		datafile << endl;
 	}
 
 	datafile.close();
+}
+
+void Database::insertTable(string file, map<string, string> values)
+{
+	fstream datafile;
+	datafile.open(file, ios::in | ios::app);
+	string headers;
+
+	if (!datafile)
+	{
+		cout << "Issue connecting to database" << endl;
+	}
+	else
+	{
+		getline(datafile, headers);
+		string delimiter = ",";
+		int start = 0;
+		int end = headers.find(delimiter);
+		cout << values.size();
+		for (int i = 0; i < end; i++)
+		{
+			string header = headers.substr(start, end - start);
+			if (values.find(header) == values.end())
+			{
+				datafile << ",";
+			}
+			else
+			{
+				datafile << values.at(header) << ",";
+			}
+			
+			start = end + delimiter.size();
+			end = headers.find(delimiter, start);
+		}
+		datafile << endl;
+		datafile.close();
+	}
 }
--- CovidRecommendationAppTests/DatabaseTests.cpp
+++ CovidRecommendationAppTests/DatabaseTests.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <map>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace CovidRecommendationApp;
@@ -20,13 +21,30 @@ namespace CovidRecommendationAppTests
 		{
 			Database database;
 			string actual;
-			string expected = "PatientID\tName\tDate of Birth\tAddress\tVisited Location\tDate/Time\tLast Overseas Travel\tCovid Test\tStatus\t";
+			string expected = "PatientID,Name,Date of Birth,Address,LocationID,Date/Time,Last Overseas Travel,Covid Test,Status,";
 			string file = "PatientDetailTable.txt";
-			vector<string> headers = {"PatientID", "Name", "Date of Birth", "Address", "Visited Location", "Date/Time", "Last Overseas Travel", "Covid Test", "Status"};
+			vector<string> headers = {"PatientID", "Name", "Date of Birth", "Address", "LocationID", "Date/Time", "Last Overseas Travel", "Covid Test", "Status"};
 			database.createTable(file, headers);
 			ifstream myfile(file);
 			getline(myfile, actual);
 			Assert::IsTrue(myfile.is_open());
+			Assert::AreEqual(actual, expected);
+			myfile.close();
+		}
+
+		TEST_METHOD(TestInsertTable)
+		{
+			Database database;
+			string actual;
+			string expected = "1,Sam,26/08/1999,some st,1,16/10/2021 22:32:26,No,Negative,Alive,";
+			string file = "PatientDetailTable.txt";
+			map<string, string> values { {"PatientID", "1"}, {"Name", "Sam"}, {"Date of Birth", "26/08/1999"}, {"Address", "some st"}, {"LocationID", "1"}, {"Date/Time", "16/10/2021 22:32:26"}, {"Last Overseas Travel", "No"}, {"Covid Test", "Negative"}, {"Status", "Alive"} };
+			vector<string> headers = { "PatientID", "Name", "Date of Birth", "Address", "LocationID", "Date/Time", "Last Overseas Travel", "Covid Test", "Status" };
+			database.createTable(file, headers);
+			database.insertTable(file, values);
+			ifstream myfile(file);
+			getline(myfile, actual);
+			getline(myfile, actual);
 			Assert::AreEqual(actual, expected);
 			myfile.close();
 		}
